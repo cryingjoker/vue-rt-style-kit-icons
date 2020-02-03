40ca4e8ce8164d7506a1136b3e2b5422f3bcb3ca
Viktor Sidoretc <silrage@MacBook-Pro-Viktor.local>
2020-02-03 17:06:03 +0300

    RTRU-6061 - Поправил верстку иконки скорости

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -6,20 +6,16 @@
     <div
       v-if="caption && isSpeedIcon"
       class="icon-speed__spinned-arrow"
-      style="position: absolute; left: 0; top: 0;"
     >
       <svg
         version="1.1"
         viewBox="0 0 52 52"
       >
-        <g fill="none" stroke="#000" stroke-width="2">
-          <path pid="0" d="M46.2 24.9c.6 10.9-7.7 20.3-18.7 20.9h-.1c-10.9 1-20.6-7.1-21.5-18S13 7.2 23.9 6.2c.4 0 .8-.1 1.2-.1 4-.2 7.9.7 11.3 2.7 2.9 1.7 5.4 4.2 7.1 7.1 1.5 2.8 2.5 5.9 2.7 9z" stroke-linejoin="round" stroke-dasharray="2.9739,2.9739" class="__web-inspector-hide-shortcut__"></path>
-          <path pid="1" d="M37.4 9.5c6.1 4 9.4 11.1 8.6 18.4" stroke-linecap="round" stroke-linejoin="round" class="__web-inspector-hide-shortcut__"></path>
-          <ellipse pid="2" cx="25.9" cy="26" rx="12.8" ry="12.7" stroke-miterlimit="10" class=""></ellipse>
-          <ellipse pid="3" cx="25.9" cy="26" rx="7" ry="12.7" stroke-miterlimit="10" class=""></ellipse>
-          <path pid="4" stroke-miterlimit="10" d="M25.9 13.2v25.5M13.2 26.2h25.5M15.1 19.6h21.5M15.5 32.7h21.1"></path>
+        <g fill="none" stroke="#000" stroke-width="1">
+          <ellipse cx="25.9" cy="26" rx="12.8" ry="12.7" stroke-miterlimit="10"></ellipse>
+          <ellipse cx="25.9" cy="26" rx="7" ry="12.7" stroke-miterlimit="10"></ellipse>
+          <path stroke-miterlimit="10" d="M25.9 13.2v25.5M13.2 26.2h25.5M15.1 19.6h21.5M15.5 32.7h21.1"></path>
         </g>
-        <path pid="5" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M50.1 23.5L46 27.9l-4.4-4.2"></path>
       </svg>
       <svg
         version="1.1"
